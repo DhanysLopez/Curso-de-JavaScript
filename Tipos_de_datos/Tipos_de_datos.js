283bc5ad326a8ca6c4a6f65f6c5df8895f2dce05
DhanysLopez <67067917+DhanysLopez@users.noreply.github.com>
2021-07-22 15:14:09 -0500

    undefined, null, NaN

--- Tipos_de_datos/Tipos_de_datos.js
+++ Tipos_de_datos/Tipos_de_datos.js
@@ -13,3 +13,69 @@ console.log(apellido.toUpperCase()); // Metodo "toUpperCase"
 console.log(apellido.toLowerCase()); // Metodo "toLowerCase"
 console.log(js.includes("bajo nivel")); // Metodo includes
 console.log(js.includes("alto nivel"));
+
+/* Template Strings */
+let topic_0004 = "~Template Strings~";
+console.log(topic_0004);
+
+let user = "Synahd";
+let id = "D-5258799";
+
+//Concatenación
+let identificate = "Hola mi usuario es" + " " + user + " " + "y mi id es" + " " + id;
+console.log(identificate);
+
+//Template String
+let identificate2 = `Hola mi usuario es ${user} y mi id es ${id}`;
+console.log(identificate2);
+
+let ul = `
+    <ul>
+        <li>user 1</li>
+        <li>user 2</li>
+        <li>user 3</li>
+    </ul>
+`;
+console.log(ul)
+
+/* ~Numbers~ */
+let topic_0005 = "~Numbers~";
+console.log(topic_0005);
+
+let a = 1;
+let b = new Number(2);
+let c = 2.26;
+let d = "3";
+
+console.log(a,b);
+console.log(c.toFixed(1));
+console.log(parseInt(c));
+console.log(parseFloat(c));
+console.log(typeof c, typeof d);
+
+
+/* ~Booleans~ */
+let topic_0006 = "~Boleans~"
+console.log(topic_0006);
+
+let verdadero = true;
+let falso = false;
+let z = Boolean(true);
+let f = Boolean(false);
+
+console.log(verdadero, falso, z, f);
+console.log(typeof verdadero, typeof falso);
+
+
+/* ~undefined, null, & NaN~ */
+let topic_0007 = "~undefined, null, & NaN~";
+console.log(topic_0007);
+
+let indefinida;
+console.log(indefinida);
+
+let nulo= null;
+console.log(nulo);
+
+let noEsUnNumero = "hola" * 3.7;
+console.log(noEsUnNumero);
