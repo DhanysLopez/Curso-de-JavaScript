8a9f582756fef3cc55500aa159eca22036dfa5ce
DhanysLopez <67067917+DhanysLopez@users.noreply.github.com>
2021-07-25 15:49:07 -0500

    Tipos de Operadores

---
+++ Estructuras de Control/Estructuras_de_control.js
@@ -0,0 +1,39 @@
+/* ~Tipos de Operadores~ */
+let tiposDeOperadores = "~Tipos de Operadores~";
+console.log(tiposDeOperadores);
+
+// Operadores Aritmeticos
+let ñ = 5 + 5 - 10 * 3;
+console.log(ñ);
+
+let modulo = 5%2;
+console.log(modulo)
+
+// Operadores Relacionales
+console.log(8 > 9);
+console.log(9 > 8);
+console.log(8 >= 9);
+console.log(9 >= 8);
+console.log(7 < 7);
+console.log(7 <= 7);
+console.log(7 == 7);
+console.log("7" === 7);
+console.log(0 === false);
+console.log(7 === 7);
+
+//Incremento
+let s = 1;
+s = s + 2;
+console.log(s);
+
+// Opereadores Logicos
+
+// ! - Not
+console.log(!true);
+console.log(!false);
+
+// || - Or
+console.log(9===9) ||("9"===9);
+
+// && - And
+console.log(9===9) && ("9"===9);
