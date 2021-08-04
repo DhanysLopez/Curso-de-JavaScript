5bb0ecd701ce9196dff98e35a484ea572282a558
DhanysLopez <67067917+DhanysLopez@users.noreply.github.com>
2021-08-04 14:06:50 -0500

    aler, confirm y prompt

--- Objetos_y_Funciones_del_Lenguaje/objetivos_y_funciones_del_lenguaje.js
+++ Objetos_y_Funciones_del_Lenguaje/objetivos_y_funciones_del_lenguaje.js
@@ -75,3 +75,83 @@ let x = 4,
     y = 2,
     pruebaXY = "Se espera que X siempre sea menor que Y";
 console.assert(x < y, (x,y, pruebaXY));
+
+/* ~Objeto - Date~ */
+let objetoDate = "~Objeto - Date~";
+console.log(objetoDate);
+
+console.log(Date);
+
+let fecha = new Date();
+console.log(fecha); 
+console.log(fecha.getDate());
+console.log(fecha.getDay());
+console.log(fecha.getMonth());
+console.log(fecha.getYear());
+console.log(fecha.getFullYear());
+console.log(fecha.getHours());
+console.log(fecha.getMinutes());
+console.log(fecha.getSeconds());
+console.log(fecha.getMilliseconds());
+console.log(fecha.toString());
+console.log(fecha.toDateString());
+console.log(fecha.toLocaleDateString());
+console.log(fecha.getTimezoneOffset());
+console.log(fecha.getUTCDate());
+console.log(fecha.getUTCHours());
+console.log(Date.now());
+
+let cumpledhan = new Date(1999,2,10);
+console.log(cumpledhan);
+
+/* ~Objeto - Math~ */
+let objetoMath = "~Objeto - Math~";
+console.log(objetoMath);
+
+console.log(Math);
+console.log(Math.PI);
+console.log(Math.abs(-8.7));
+console.log(Math.ceil(7.2));
+console.log(Math.floor(7.8));
+console.log(Math.round(7.2));
+console.log(Math.sqrt(81));
+console.log(Math.pow(2,5));
+console.log(Math.sign(6.9));
+console.log(Math.random());
+console.log(Math.random() * 100);
+console.log(Math.round(Math.random() * 100));
+
+/* ~Operador de Cortocircuito~ */
+let operadorDeCorticircuito = "~Operadores de Cortocircuito~"
+console.log(operadorDeCorticircuito);
+
+/*
+Cortocircuito - OR - cuando el valor de la izquierda
+en la expresión siempre pueda validae a true, es el 
+valor que se cargara por defecto
+*/
+
+/*
+Cortocircuito - AND - cuando el valor de la izquierda
+en la expresión siempre pueda validar a false, es el
+valor que se cargará por defecto
+*/
+
+function saludar(nombre) {
+    nombre = nombre || "Desconocido";
+    console.log(`Hola ${nombre}`)
+}
+
+saludar("Dhanys");
+saludar();
+
+
+/* ~alert, confirm y prompt~ */
+let alertConfimYPrompt = "~alert, confirm y prompt~"
+
+let alerta = alert("Hola esto es una alerta"),
+    confirmacion = confirm("Hola esto es una confirmación"),
+    aviso = prompt("Hola esto es un aviso");
+console.log(alerta);
+console.log(confirmacion);
+console.log(aviso);
